886c000d6c63154db16735b8967c9d44b8cda061
EC2 Default User <ec2-user@ip-172-31-18-252.ap-northeast-1.compute.internal>
2020-11-19 04:57:24 +0000

    application.html修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -40,9 +40,9 @@ class Users::SessionsController < Devise::SessionsController
 
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
 
   # protected
 
