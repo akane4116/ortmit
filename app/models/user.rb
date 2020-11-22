eeba3b402ee1312c36a33472ba0ebe115b536f98
EC2 Default User <ec2-user@ip-172-31-18-252.ap-northeast-1.compute.internal>
2020-11-22 13:33:42 +0000

    usersvalidate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true
+  validates :email, presence: true
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable,
          invite_for: 24.hours
