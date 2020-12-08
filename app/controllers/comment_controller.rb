b643a29dade9ad3c8aabdc2ab08163bde363df2a
EC2 Default User <ec2-user@ip-172-31-18-252.ap-northeast-1.compute.internal>
2020-12-08 12:46:39 +0000

    question/indexでnameが表示されるように修正

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,14 +1,28 @@
 class CommentController < ApplicationController
+    def index
+        @topic = Topic.find_by(:params[topic_id])
+    end
+    
+    
     def new
      @comment = Comment.new
+     @topic = Topic.find_by(id: params[:topic_id])
     end
     
     def create
-     @comment = Comment
+        @topic = Topic.find(params[:topic_id])
+        @commemt = @topic.comments.new(comment_params)
+        @comment.user_id = current_user.id
+        @comment.content = params[:comment][:content]
     end
     
     def destroy
       super
     end
     
+    private
+     def comment_params
+        params.require(:comment).permit(:name,:content)
+        
+     end
 end
