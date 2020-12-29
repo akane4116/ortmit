2b2ace9a23b37ceab8b9fc86802405c728928b93
EC2 Default User <ec2-user@ip-172-31-18-252.ap-northeast-1.compute.internal>
2020-12-29 11:34:13 +0000

    test2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,9 +94,9 @@ Rails.application.configure do
   domain: 'gmail.com',
   port:587,
   #gmailのユーザアカウント（xxxx@gmail.com)※念のため、credentials.yml.enc行き
-  user_name: Rails.application.credentials.gmail[:mail_address],
+  mail_address: Rails.application.credentials.gmail[:mail_address],
   #gmail２段階認証回避のためにアプリケーションでの利用パスワードを取得、必ずcredentials.yml.endに設定を！！
-  password: Rails.application.credentials.gmail[:app_password],
+  app_password: Rails.application.credentials.gmail[:app_password],
   #パスワードをBase64でエンコード
   authentication: :login
   }
