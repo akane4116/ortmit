823eec9aadc65c6ae722f1206d4209c82faeb51a
EC2 Default User <ec2-user@ip-172-31-18-252.ap-northeast-1.compute.internal>
2020-12-29 11:44:51 +0000

    test5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,17 +89,16 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   #送信方法として:smtpを指定した場合は、このconfigを使って送信詳細の設定を行います
   config.action_mailer.smtp_settings = {
-  #gmail利用時はaddress,domain,portは下記で固定
-  address:"smtp.gmail.com",
-  domain: 'gmail.com',
-  port:587,
-  #gmailのユーザアカウント（xxxx@gmail.com)※念のため、credentials.yml.enc行き
-  mail_address: Rails.application.credentials.gmail[:mail_address],
-  #gmail２段階認証回避のためにアプリケーションでの利用パスワードを取得、必ずcredentials.yml.endに設定を！！
-  app_password: Rails.application.credentials.gmail[:app_password],
-  #パスワードをBase64でエンコード
+ address:"smtp.gmail.com",
+ domain: 'gmail.com',
+ port:587,
+ #gmailのユーザアカウント（xxxx@gmail.com)※念のため、credentials.yml.enc行き
+ mail_address: Rails.application.credentials.gmail[:mail_address],
+ #gmail２段階認証回避のためにアプリケーションでの利用パスワードを取得、必ずcredentials.yml.endに設定を！！
+ app_password: Rails.application.credentials.gmail[:app_password],
+ #パスワードをBase64でエンコード
   authentication: :login
-  }
+ }
   
   config.require_master_key = true
   
